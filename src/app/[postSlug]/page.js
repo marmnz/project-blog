4ccd1d6baed4c554144ee9f7a1067828311e3466
Marina Marinova <marinavmarinova@gmail.com>
2025-10-20 01:50:07 +0300

    Refactor BlogPost component to use dynamic frontmatter data and improve code formatting

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
 
-function BlogPost() {
+import { readFile } from "fs/promises";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+async function BlogPost({ params }) {
+  const { postSlug } = await params;
+  const fileContent = await readFile(
+    `${process.cwd()}/content/${postSlug}.mdx`,
+    "utf-8"
+  );
+
+  const { data: frontmatter, content } = await matter(fileContent);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
